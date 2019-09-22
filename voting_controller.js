d2ce03be36cb2f92223c0fc9f943780660f7988c
kemblyqa <kquirosaraya@gmail.com>
2019-09-22 16:11:35 -0600

    funcionalidad privacidad lista para probar

--- voting_controller.js
+++ voting_controller.js
@@ -1,22 +1,36 @@
-
 var container_elem = document.getElementById("container");
-container_elem.onmouseup = (event) => {
-    var current_opacity = window.getComputedStyle(container_elem, ":active").getPropertyValue("opacity");
+var privateEventTimer;
+
+container_elem.addEventListener("mouseup", (e)=>{
+    e.preventDefault();
+    var current_opacity = window.getComputedStyle(container_elem, null).getPropertyValue("opacity");
+    console.log("mouseup event", container_elem.style.opacity);
     container_elem.style.opacity = current_opacity;
+    console.log(current_opacity);
     eventMouseClick();
-}
-
-container_elem.onclick = (event) => {
-    
-}
-
+})
+container_elem.addEventListener("mousedown", (e)=>{
+    e.preventDefault();
+    container_elem.style.transition = "opacity 2s ease-in-out";
+    container_elem.style.opacity = 0;
+})
+container_elem.addEventListener("click", (e)=>{
+    e.preventDefault();
+    eventMouseClick();    
+})
 function eventMouseClick() {
-    var timer = setInterval(()=>{
-
+    clearTimeout(privateEventTimer);
+    privateEventTimer = setTimeout(()=>{
+        revertPrivateMode();
     }, 5000);
 };
 
-    /** 
+function revertPrivateMode(){
+    container_elem.style.transition = "opacity 1s ease-in-out";
+    container_elem.style.opacity = 0.8;
+}
+
+/** 
  * Function that create a interface based in a json file.
  * @param {*} settings json to set the interface.
  */
